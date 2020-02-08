8d003d13f42768bce04795d6d96f324828390bfe
viper3400 <viper_3400@gmx.de>
2020-02-08 11:49:27 +0100

    Added FileDateComparer

---
+++ Jaxx.Net.Helpers.IO.Tests/FileDateComparerShould.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Jaxx.Net.Helpers.IO.Tests
+{
+    public class FileDateComparerShould
+    {
+        private readonly ITestOutputHelper output;
+        public FileDateComparerShould(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void EvaluateFileAgeByLastWriteTime()
+        {
+            // clear previous test run
+            var tmpPath = Path.Join(Path.GetTempPath(), "Jaxx.Net.Helpers.IO.Tests","EvaluateFileAgeByLastWriteTime");
+            if (Directory.Exists(tmpPath)) Directory.Delete(tmpPath, true);
+
+            // init test with new directory and new file
+            Directory.CreateDirectory(tmpPath);
+            var fileName = "myfile.txt";
+            //create first file (old one)
+            var oldFilePath = Path.Join(tmpPath, fileName);
+            File.WriteAllText(oldFilePath, "I am the old one!");
+            // now create a new one (but wait a second)
+            System.Threading.Thread.Sleep(1000);
+            var newFilePath = Path.Join(tmpPath, "newfile.txt");
+            File.WriteAllText(newFilePath, "I am the new one!");
+
+            var file1 = new FileInfo(oldFilePath);
+            var file2 = new FileInfo(newFilePath);
+
+            var comparer = new FileDateComparer(file2, file1, CompareDateOption.LastWriteTime);
+            output.WriteLine($"TC1: OldFile: {comparer.OldFile}, NewFile: {comparer.NewFile}");
+            Assert.Equal(file1, comparer.OldFile);
+            Assert.Equal(file2, comparer.NewFile);
+
+            // modify old file (but wait a second)
+            System.Threading.Thread.Sleep(1000);
+            File.AppendAllText(oldFilePath, "I may be the old one, but i've got an update!");
+
+            // refesh file info
+            file1 = new FileInfo(oldFilePath);
+            file2 = new FileInfo(newFilePath);
+
+            comparer = new FileDateComparer(file2, file1, CompareDateOption.LastWriteTime);
+            output.WriteLine($"TC2: OldFile: {comparer.OldFile}, NewFile: {comparer.NewFile}");
+            Assert.Equal(file2, comparer.OldFile);
+            Assert.Equal(file1, comparer.NewFile);
+        }
+        [Fact]
+        public void EvaluateFileAgeByCreationTime()
+        {
+            // clear previous test run
+            var tmpPath = Path.Join(Path.GetTempPath(), "Jaxx.Net.Helpers.IO.Tests", "EvaluateFileAgeByLastWriteTime");
+            if (Directory.Exists(tmpPath)) Directory.Delete(tmpPath, true);
+
+            // init test with new directory and new file
+            Directory.CreateDirectory(tmpPath);
+            var fileName = "myfile.txt";
+            //create first file (old one)
+            var oldFilePath = Path.Join(tmpPath, fileName);
+            File.WriteAllText(oldFilePath, "I am the old one!");
+            // now create a new one
+            var newFilePath = Path.Join(tmpPath, "newfile.txt");
+            File.WriteAllText(newFilePath, "I am the new one!");
+
+            var file1 = new FileInfo(oldFilePath);
+            var file2 = new FileInfo(newFilePath);
+
+            var comparer = new FileDateComparer(file2, file1, CompareDateOption.LastWriteTime);
+            Assert.Equal(file1, comparer.OldFile);
+            Assert.Equal(file2, comparer.NewFile);
+        }
+    }
+}
---
+++ Jaxx.Net.Helpers.IO/FileDateComparer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Text;
+
+namespace Jaxx.Net.Helpers.IO
+{
+    public class FileDateComparer
+    {
+        public FileDateComparer(FileInfo file1, FileInfo file2, CompareDateOption compareDateOption)
+        {
+            Contract.Requires(file1 != null);
+            Contract.Requires(file2 != null);
+
+            switch (compareDateOption)
+            {
+                case CompareDateOption.CreationTime:
+                    if (file1.CreationTimeUtc > file2.CreationTimeUtc)
+                    {
+                        OldFile = file2;
+                        NewFile = file1;
+                    } 
+                    else if (file1.CreationTimeUtc <= file2.CreationTimeUtc)
+                    {
+                        OldFile = file1;
+                        NewFile = file2;
+                    }
+                    break;
+                case CompareDateOption.LastWriteTime:
+                    if (file1.LastWriteTimeUtc > file2.LastWriteTimeUtc)
+                    {
+                        OldFile = file2;
+                        NewFile = file1;
+                    }
+                    else if (file1.LastWriteTimeUtc <= file2.LastWriteTimeUtc)
+                    {
+                        OldFile = file1;
+                        NewFile = file2;
+                    }
+                    break;
+            }
+        }
+
+        public FileInfo OldFile { get; private set; }
+        public FileInfo NewFile { get; private set; }
+    }
+}
